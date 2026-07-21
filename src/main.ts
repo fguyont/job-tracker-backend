7bbea93b302769ce87ff890499c9c1d43fffc0ac
Frederic Guyont <Frederic_Guyont@fakemail.com>
2026-07-21 09:47:59 +0200

    ADD from front to back CORS enabling

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Autorise le frontend (Vite) à appeler l'API
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
