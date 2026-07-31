4205e15bf932ba4728fe43d826034d79d52a1831
Frederic Guyont <guyont.frederic@gmail.com>
2026-07-31 17:04:29 +0200

    ADD userId parameter in job-application controller and services

--- src/job-applications/job-applications.controller.ts
+++ src/job-applications/job-applications.controller.ts
@@ -3,36 +3,36 @@ import { JobApplicationsService } from './job-applications.service';
 import { CreateJobApplicationDto } from './dto/create-job-application.dto';
 import { UpdateJobApplicationDto } from './dto/update-job-application.dto';
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { GetUser } from '@/auth/decorators/get-user.decorator';
 
 @UseGuards(JwtAuthGuard)
 @Controller('job-applications')
 export class JobApplicationsController {
-  constructor(private readonly jobApplicationsService: JobApplicationsService) {}
+  constructor(private readonly jobApplicationsService: JobApplicationsService) { }
 
   @Post()
-  create(@Body() createJobApplicationDto: CreateJobApplicationDto) {
-    const userId = '10000000-0000-0000-0000-000000000000'; // Using a fake but existing user in the database to create a job application
+  create(@Body() createJobApplicationDto: CreateJobApplicationDto, @GetUser('id') userId: string) {
     return this.jobApplicationsService.create(createJobApplicationDto, userId);
   }
 
   @Get()
-  findAll() {
-    return this.jobApplicationsService.findAll();
+  findAll(@GetUser('id') userId: string) {
+    return this.jobApplicationsService.findAll(userId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.jobApplicationsService.findById(id);
+  findOne(@Param('id') id: string, @GetUser('id') userId: string) {
+    return this.jobApplicationsService.findById(id, userId);
   }
 
   @Patch(':id')
-  archive(@Param('id') id: string) {
-    return this.jobApplicationsService.setArchived(id);
+  archive(@Param('id') id: string, @GetUser('id') userId: string) {
+    return this.jobApplicationsService.setArchived(id, userId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJobApplicationDto: UpdateJobApplicationDto) {
-    return this.jobApplicationsService.update(id, updateJobApplicationDto);
+  update(@Param('id') id: string, @GetUser('id') userId: string, @Body() updateJobApplicationDto: UpdateJobApplicationDto) {
+    return this.jobApplicationsService.update(id, userId, updateJobApplicationDto);
   }
-  
+
 }
--- src/job-applications/job-applications.service.ts
+++ src/job-applications/job-applications.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateJobApplicationDto, JobApplicationStatus } from './dto/create-job-application.dto';
 import { UpdateJobApplicationDto } from './dto/update-job-application.dto';
 import { PrismaService } from '../../prisma/prisma.service';
@@ -19,40 +19,54 @@ export class JobApplicationsService {
     });
   }
 
-  async findAll() {
+  async findAll(userId: string) {
     return await this.prisma.jobApplication.findMany({
+      where: {
+        userId: userId
+      },
       orderBy: {
         createdAt: 'desc',
       },
     });
   }
 
-  async findById(id: string) {
+  async findById(id: string, userId: string) {
     return await this.prisma.jobApplication.findUniqueOrThrow({
-      where: { id },
+      where: {
+        id: id,
+        userId: userId
+      },
     });
   }
 
-  async setArchived(id: string) {
-    var toArchived = await this.findById(id);
+  async setArchived(id: string, userId: string) {
+    var toArchived = await this.findById(id, userId);
     if (toArchived != null) {
       return await this.prisma.jobApplication.update({
-        where: { id },
+        where: {
+          id: id,
+          userId: userId
+        },
         data: {
           status: JobApplicationStatus.ARCHIVED,
         },
       });
     }
+    throw new BadRequestException(`Job application not archived`);
   }
 
-  async update(id: string, updateJobApplicationDto: UpdateJobApplicationDto) {
-    var toUpdate = await this.findById(id);
+  async update(id: string, userId: string, updateJobApplicationDto: UpdateJobApplicationDto) {
+    var toUpdate = await this.findById(id, userId);
     if (toUpdate != null) {
       return await this.prisma.jobApplication.update({
-        where: { id },
+        where: {
+          id: id,
+          userId: userId
+        },
         data: updateJobApplicationDto,
       });
     }
+    throw new BadRequestException(`Job application not updated`);
   }
 
 }
